18c000fa797558117476c03be22269f9a92ff46f
julianbaumann-msdos <julian-baumann@outlook.com>
2021-04-06 12:57:46 -0700

    Stuff

--- REMTS/DataService.cs
+++ REMTS/DataService.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Collections.ObjectModel;
+using RemoteExecuter.Entities;
+using RemoteExecuter;
 
 namespace REMTS
 {
-    class DataService
+    public class DataService
     {
+        private DataService _instance;
+
+        private DeliveryOptimization _deliveryOptimization;
+
+        public DataService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DataService();
+                }
+
+                return _instance;
+            }
+        }
+
+        public DataService()
+        {
+            _deliveryOptimization = new DeliveryOptimization();
+        }
+
+
+        public RemotePcInfo[] ComputerList { get; set; }
+
+        public void RunDeliveryOptimization()
+        {
+            foreach (RemotePcInfo pc in ComputerList)
+            {
+                DeliveryOptimizationItem[] items = _deliveryOptimization.GetDataFromRemotePC(pc);
+            }
+
+        }
     }
 }
